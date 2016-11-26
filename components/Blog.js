ebfc17636af8d1622af6aa699e12c50477cf9b61
jkalfayan <jkalfayan@apple.com>
2016-11-25 20:45:59 -0800

    added basic html for the main page

--- components/Blog.js
+++ components/Blog.js
@@ -1,8 +1,22 @@
 var React = require('react');
+var jonImage = require('../styles').jonImage;
 
 function Blog(props) {
     return (
-        <div> Testing 1, 2, 3 </div>
+        <div className="container">
+            <div className="row" >
+                <div className="col-sm-12 text-center"><h1>Jonks Blog!</h1></div>
+            </div>
+            <div className="row">
+                <div className="col-sm-1">
+                    <img src="https://avatars3.githubusercontent.com/u/1980266?v=3&s=460" style={jonImage}></img>
+                </div>
+                <div className="col-sm-10 col-sm-offset-1">
+                    <h3>A blog post!</h3>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
---
+++ styles/index.js
@@ -0,0 +1,9 @@
+var styles = {
+    jonImage: {
+        borderRadius: '10px 10px 10px 10px',
+        width: '60px',
+        height: '60px'
+    }
+};
+
+module.exports = styles;
