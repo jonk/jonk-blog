b2c5398efe8d0b3a80a427468201ca9503ac4caa
Jon Kalfayan <jonk1993@gmail.com>
2016-12-05 20:53:07 -0800

    dangerously setting innerhtml lol, and styling

--- app/components/BlogPost.js
+++ app/components/BlogPost.js
@@ -2,10 +2,23 @@ import React, {PropTypes, Component} from 'react';
 
 export default class BlogPost extends Component {
     render() {
+        const blogHeader = {
+            color: "#93291b",
+            fontFamily: "Helvetica Neue",
+            fontSize: 36,
+            paddingBottom: 10,
+            marginLeft: 5
+        };
+        const blogParagraph = {
+            fontFamily: "Verdana",
+            marginBottom: 15,
+            marginLeft: 20,
+            color: "#666"
+        }
         return (
             <div>
-                <h3>{this.props.title}</h3>
-                <p>{this.props.content}</p>
+                <h3 style={blogHeader}>{this.props.title}</h3>
+                <p style={blogParagraph} dangerouslySetInnerHTML={{__html: this.props.content }} />
             </div>
         )
     }
--- app/components/Title.js
+++ app/components/Title.js
@@ -2,8 +2,18 @@ import React, {Component} from 'react';
 
 export default class Title extends Component {
     render() {
+        const titleStyle = {
+           color: "#333333",
+           fontFamily: "Helvetica Neue",
+           fontSize: 64,
+           fontWeight: "normal",
+           margin: "10 0 54",
+           textAlign: "center"
+        };
         return (
-            <h1>Jonk Blog</h1>
+            <div>
+                <h1 style={titleStyle}>Jonk Blog</h1>
+            </div>
         )
     }
 }
